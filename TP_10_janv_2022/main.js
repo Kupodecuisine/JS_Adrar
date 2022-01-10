8e355c1a632a29e95d96ddd9490d8cd6b74ba89d
Kupodecuisine <87529665+Kupodecuisine@users.noreply.github.com>
2022-01-10 14:05:03 +0100

    azeazeazeaze

--- TP_10_janv_2022/main.js
+++ TP_10_janv_2022/main.js
@@ -9,31 +9,26 @@ let nbrCompte = document.querySelector("#nbrCompte");
 
 function ajouter() {
 	
-	let inputs = document.querySelectorAll("input");
-	let name = inputs[0].value;
-	let firstName = inputs[1].value;
-	let login = inputs[2].value;
-	let mdp = inputs[3].value;
+	let name = document.querySelector("#name").value;
+	let firstName = document.querySelector("#firstName").value;
+	let login = document.querySelector("#login").value;
+	let mdp = document.querySelector("#mdp").value;
+	error.innerHTML = "";
 	
 	if(name != "" && firstName != "" && login != "" && mdp != ""){
 		
-		tab.push(name, firstName, login, mdp);
+		tab.push([name, firstName, login, mdp]);
 		
 		// console.log(tab);
 		
-		resultat.innerHTML += `<p> ${tab[0]} ${tab[1]} ${tab[2]} ${tab[3]} </p>` ;
+		resultat.innerHTML += `<p> ${tab[cpt][0]} ${tab[cpt][1]} ${tab[cpt][2]} ${tab[cpt][3]} </p>` ;
 	
 		cpt++;
 		
 		nbrCompte.innerHTML = `il y a ${cpt} comptes utilisateurs`;
 		
+		console.log(tab);
 	}
-	
-	name = "";  
-	firstName = "";	
-	login = ""; 
-	mdp = "";
-	tab = [];
 	
 };
 
